d47ef828289450221df83fd29475bcfb99181092
Pran2805 <pranavshinde.as@gmail.com>
2026-03-23 12:16:42 +0530

    feat: resizing issue solved

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 // Mesh initialization
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "burlywood"});
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "burlywood" });
 const cubeMesh = new THREE.Mesh(
   cubeGeometry,
   cubeMaterial
@@ -14,23 +14,28 @@ scene.add(cubeMesh);
 
 
 // camera initialization
-// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const aspectRatio = window.innerWidth / window.innerHeight
-const camera = new THREE.OrthographicCamera(-1* aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 10000);
+// const aspectRatio = window.innerWidth / window.innerHeight
+// const camera = new THREE.OrthographicCamera(-1* aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 10000);
 camera.position.z = 5;
 scene.add(camera);
 
 const canvas = document.querySelector("#canvas");
 
 // renderer initialization
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({ canvas });
 
-renderer.setSize(window.innerWidth, window.innerHeight);
 const controls = new OrbitControls(camera, renderer.domElement);
 
-function animate(){
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix()
+})
+
+function animate() {
   controls.update()
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.render(scene, camera);
   window.requestAnimationFrame(animate)
 }
